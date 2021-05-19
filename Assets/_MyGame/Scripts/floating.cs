1ef7da50fa588ba6364fbfa0f0f2bdeded9b9a48
MaxStienitzka <maximilian.stienitzka03@htl-salzburg.ac.at>
2021-05-19 22:53:41 +0200

    added normals

---
+++ Assets/_MyGame/Scripts/floating.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class floating : MonoBehaviour
+{
+    public GameObject levítationObject;
+    public AnimationCurve myCurve;
+
+    void Update()
+    {
+        levítationObject.transform.position = new Vector3(levítationObject.transform.position.x, myCurve.Evaluate((Time.time % myCurve.length)), levítationObject.transform.position.z);
+    }
+}
+ 
